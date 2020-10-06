63fd5a24ff1284d55b5d18a712815d17c22839ca
ResurgentSeven <72426552+ResurgentSeven@users.noreply.github.com>
2020-10-06 14:53:15 -0400

    Update thetaBetaMach.m
    Just replaced some symbolic solving with the final equation to speed things up

--- thetaBetaMach.m
+++ thetaBetaMach.m
@@ -19,25 +19,24 @@ function [theta_Out, beta_Out, mach_Out] = thetaBetaMach(theta_In, beta_In, mach
     if argGood
         % assumptions when dealing with air
         gamma = 1.4;
-        
-        % eq describing relationship between theta beta and mach
-        syms myTheta myBeta mach
-        eq = tand(myTheta) == 2*(cotd(myBeta))*(((mach^2)*((sind(myBeta))^2) - 1)/((mach^2)*(gamma + cosd(2*myBeta)) + 2));
-        
+               
         % find the mach number from inputs
         i = find(myArgs == 0);
         switch i
             case 1
                 % theta wasn't inputted
-                eq = subs(eq, [myBeta, mach], [beta_In, mach_In]);
-                theta_Out = double(vpasolve(eq, myTheta, [0, 46]));
+                
+                theta_Out = atand(2*(cotd(beta_In))*(((mach_In^2)*((sind(beta_In))^2) - 1)/((mach_In^2)*(gamma + cosd(2*beta_In)) + 2)));
                 
                 beta_Out = beta_In;
                 mach_Out = mach_In;
                 
             case 2
                 % beta wasn't inputted
-                eq = subs(eq, [myTheta, mach], [theta_In, mach_In]);
+                
+                syms myBeta
+                eq = tand(theta_In) == 2*(cotd(myBeta))*(((mach_In^2)*((sind(myBeta))^2) - 1)/((mach_In^2)*(gamma + cosd(2*myBeta)) + 2));
+                
                 beta_Out = double(vpasolve(eq, myBeta, [0, 90]));
                 
                 theta_Out = theta_In;
@@ -45,9 +44,9 @@ function [theta_Out, beta_Out, mach_Out] = thetaBetaMach(theta_In, beta_In, mach
                 
             case 3
                 % mach wasn't inputted
-                eq = subs(eq, [myBeta, myTheta], [beta_In, theta_In]);
-                mach_Out = double(vpasolve(eq, mach, [0, inf]));
                 
+                mach_Out = 10^(1/2)*(-(cot((pi*beta_In)/180) + tan((pi*theta_In)/180))/(7*tan((pi*theta_In)/180) + 5*cos((pi*beta_In)/90)*tan((pi*theta_In)/180) - 10*cot((pi*beta_In)/180)*sin((pi*beta_In)/180)^2))^(1/2);
+                                
                 beta_Out = beta_In;
                 theta_Out = theta_In;
                 
